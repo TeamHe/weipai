0467b31bda049aa218af8f1c07f6a2e976979a3b
rui he <herui13@126.com>
2023-07-27 22:40:40 +0800

    新增南网数据处理基类 nw_data_base

---
+++ ResModel/nw/nw_data_base.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ResModel.nw
+{
+    public abstract class nw_data_base
+    {
+        public DateTime DataTime { get; set; }
+
+        public abstract int PackLength { get; }
+
+        public nw_data_base() { }
+
+        public abstract int Decode(byte[] data, int offset);
+
+        public abstract int Encode(byte[] data, int offset);
+    }
+}
