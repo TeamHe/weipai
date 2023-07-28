a9f9d65d424b06794c6181094f517082d7626cfb
rui he <herui13@126.com>
2023-07-28 23:01:53 +0800

    南网: 新增数据处理指令指令码: 0x40——工作电能量状态数据

---
+++ GridBackGround/CommandDeal/nw/nw_cmd_48_energy_status.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using ResModel;
+using ResModel.nw;
+using ResModel.PowerPole;
+
+namespace GridBackGround.CommandDeal.nw
+{
+    /// <summary>
+    /// 南网数据包: 工作电能量状态数据 处理
+    /// </summary>
+    public class nw_cmd_48_energy_status : nw_cmd_base
+    {
+        public override int Control { get { return 0x48; } }
+
+        public override string Name { get { return "工作电能量状态数据"; } }
+
+        /// <summary>
+        /// 帧标识
+        /// </summary>
+        public int FramFlag { get; set; }
+
+        protected int Pnum { get; set; }
+
+        protected bool Response { get; set; }
+
+        public List<nw_data_48_energy_status> values { get; set; }
+
+        public nw_cmd_48_energy_status()
+        {
+            this.values = new List<nw_data_48_energy_status> { };
+        }
+
+        public nw_cmd_48_energy_status(IPowerPole pole) : base(pole)
+        {
+            this.values = new List<nw_data_48_energy_status> { };
+        }
+
+        public override int Decode(out string msg)
+        {
+            msg = null;
+            if (this.Data == null || this.Data.Length == 0)
+            {
+                msg = "装置未上送数据";
+                return 0;
+            }
+
+            int offset = 4; //不验证密文信息，以及帧标识
+            int ret = 0;
+            this.FramFlag = this.Data[offset++];
+            this.Pnum = this.Data[offset++];
+            if (this.Pnum > 0)
+            {
+                offset += this.GetDateTime(this.Data, offset, out DateTime datatime);
+                for (int i = 0; i < Pnum; i++)
+                {
+                    nw_data_48_energy_status value = new nw_data_48_energy_status() { DataTime = datatime };
+                    if((ret = value.Decode(this.Data, offset)) < 0)
+                    {
+                        msg = string.Format("第{0}包数据解析失败", i); 
+                        return ret;
+                    }
+
+                    offset += ret;
+                    this.values.Add(value);
+                    //显示数据
+                    NewDataInfo(this.Pole, new DataInfo(DataInfoState.rec, this.Pole,
+                        this.Name, value.ToString()));
+
+                    if (i == this.Pnum - 1)
+                        break;
+                    if ((this.Data.Length - offset) < 2)
+                    {
+                        msg = string.Format("第{0}包数据长度错误", i + 1);
+                        break;
+                    }
+
+                    offset += nw_cmd_base.GetU16(this.Data, offset, out int timestramp);
+                    datatime = datatime.AddSeconds(timestramp);
+                }
+            }
+
+            this.Response = true;
+            this.SendCommand(out string msg_send);
+            msg += msg_send;
+            return 0;
+        }
+
+        public override byte[] Encode(out string msg)
+        {
+            msg = string.Empty;
+            if(this.Response)
+            {
+                return this.ResponseData(this.FramFlag);
+            }
+            else
+            {
+                msg = "主站请求上传" + this.Name;
+                return null;
+            }
+        }
+
+
+    }
+}
---
+++ ResModel/nw/nw_data_48_energy_status.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace ResModel.nw
+{
+    /// <summary>
+    /// 设备工作电能量状态
+    /// </summary>
+    public class nw_data_48_energy_status : nw_data_base
+    {
+        public override int PackLength { get { return 17; } }
+
+        /// <summary>
+        /// 电池编号
+        /// </summary>
+        public int BatteryNo { get; set; }
+
+        /// <summary>
+        /// 电池电量
+        /// </summary>
+        public int BatteryLevel { get; set; }
+
+        /// <summary>
+        /// 电池电压
+        /// </summary>
+        public double BatteryVoltage { get; set; }
+
+        /// <summary>
+        /// 电池电流
+        /// </summary>
+        public int BatteryCurrent { get; set; }
+
+        /// <summary>
+        /// 电池充电状态
+        /// </summary>
+        public bool BatteryCharge{ get; set; }
+
+        /// <summary>
+        /// 工作温度
+        /// </summary>
+        public double WorkTemp { get; set; }
+
+        /// <summary>
+        /// 光伏/风力阵列电压
+        /// </summary>
+        public double InputVoltage { get; set; }
+
+        /// <summary>
+        /// 光伏/风力阵列电流
+        /// </summary>
+        public int InputCurrent { get; set; }
+
+        /// <summary>
+        /// 负载电压
+        /// </summary>
+        public double LoadVoltage { get; set; }
+
+        /// <summary>
+        ///负载电流
+        /// </summary>
+        public int LoadCurrent { get; set; }
+
+        public override int Decode(byte[] data, int offset)
+        {
+            int val;
+            if ((data.Length - offset) < this.PackLength)
+                return -1;
+
+            this.BatteryNo = data[offset++];
+            this.BatteryLevel = data[offset++]; 
+
+            offset += nw_cmd_base.GetU16(data, offset, out val);
+            this.BatteryVoltage = val * 0.1;
+
+            offset += nw_cmd_base.GetU16(data, offset, out val);
+            this.BatteryCurrent = val;
+
+            if (data[offset++] > 0)
+                this.BatteryCharge = true;
+            else 
+                this.BatteryCharge = false;
+
+            offset += nw_cmd_base.GetU16(data, offset, out val);
+            this.WorkTemp = (val - 500) * 0.1;
+
+            offset += nw_cmd_base.GetU16(data, offset, out val);
+            this.InputVoltage = val * 0.1;
+
+            offset += nw_cmd_base.GetU16(data, offset, out val);
+            this.InputCurrent = val;
+
+            offset += nw_cmd_base.GetU16(data, offset, out val);
+            this.LoadVoltage = val * 0.1;
+
+            offset += nw_cmd_base.GetU16(data, offset, out val);
+            this.LoadCurrent = val;
+            return this.PackLength;
+        }
+
+        public override int Encode(byte[] data, int offset)
+        {
+            if ((data.Length - offset) < this.PackLength)
+                return -1;
+
+            data[offset++] = (byte)this.BatteryNo;
+            data[offset++] = (byte)this.BatteryLevel;
+            offset += nw_cmd_base.SetU16(data, offset, (int)(this.BatteryVoltage * 10));
+            offset += nw_cmd_base.SetU16(data, offset, this.BatteryCurrent);
+
+            if (this.BatteryCharge)
+                data[offset++] = 1;
+            else
+                data[offset++] = 0;
+
+            offset += nw_cmd_base.SetU16(data, offset, (int)((this.WorkTemp + 50) * 10));
+            offset += nw_cmd_base.SetU16(data, offset, (int)(this.InputVoltage * 10));
+            offset += nw_cmd_base.SetU16(data, offset, this.InputCurrent);
+            offset += nw_cmd_base.SetU16(data, offset, (int)(this.LoadVoltage * 10));
+            nw_cmd_base.SetU16(data, offset, this.LoadCurrent);
+            return this.PackLength;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("时间:{0:G} 电池编号:{1} 电池电量:{2}% " +
+                                 "电池电压:{3}V 电池电流:{4}mA 电池充电状态:{5} " +
+                                 "工作温度:{6}℃ 光伏电压:{7}V 光伏电流:{8}mA " +
+                                 "负载电压:{9}V 负载电流:{10}mA",
+                                 this.DataTime, this.BatteryNo, this.BatteryLevel,
+                                 this.BatteryVoltage, this.BatteryCurrent, this.BatteryCharge ? "充电" : "放电",
+                                 this.WorkTemp, this.InputVoltage, this.InputCurrent,
+                                 this.LoadVoltage, this.LoadCurrent);
+        }
+    }
+}
