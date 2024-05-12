e05262683ef3d39c43684e462c728f424f60664a
rui he <herui13@126.com>
2024-05-12 14:41:49 +0800

    国网:  新增心跳数据报处理(2015版规约)

---
+++ cma.service/gw_cmd/gw_cmd_stat_base.cs
@@ -0,0 +1,12 @@
+﻿using ResModel.gw;
+
+namespace cma.service.gw_cmd
+{
+    public abstract class gw_cmd_stat_base : gw_cmd_base
+    {
+        public override gw_frame_type SendFrameType { get { return gw_frame_type.ResWorkState; } }
+
+        public override gw_frame_type RecvFrameType { get { return gw_frame_type.WorkState; } }
+
+    }
+}
---
+++ cma.service/gw_cmd/gw_cmd_stat_heart.cs
@@ -0,0 +1,34 @@
+﻿using ResModel.gw;
+using System;
+
+namespace cma.service.gw_cmd
+{
+    public class gw_cmd_stat_heart : gw_cmd_stat_base
+    {
+        public override string Name { get { return "心跳数据报"; } }
+
+        public override int PType { get { return 0xe6; } }
+
+        /// <summary>
+        /// 当前时间
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        public override int decode(byte[] data, int offset, out string msg)
+        {
+            int start = offset;
+            if(data.Length -offset < 4)
+                throw new Exception("数据缓冲区长度太小");
+
+            offset += gw_coding.GetTime(data, offset, out DateTime time);
+            this.Time = time;
+            msg = string.Format("当前时间:{0}", this.Time);
+            return offset - start;
+        }
+
+        public override byte[] encode(out string msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
