803f2f38a2d4985e13405f49acfaeead254a9d2a
rui he <herui13@126.com>
2024-05-13 21:40:24 +0800

    国网: 新增故障信息报(2015版)

---
+++ ResModel/gw/gw_stat_error.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace ResModel.gw
+{
+    public class gw_stat_error
+    {
+        public gw_stat_error() { }
+
+        public DateTime Time { get; set; }
+
+        public string Desc {  get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("采集时间:{0}", this.Time);
+            sb.AppendFormat("故障描述:{0}", this.Desc);
+            return sb.ToString();
+        }
+    }
+}
---
+++ cma.service/gw_cmd/gw_stat_error.cs
@@ -0,0 +1,38 @@
+﻿using ResModel.gw;
+using System;
+
+namespace cma.service.gw_cmd
+{
+    public class gw_cmd_stat_error : gw_cmd_stat_base
+    {
+        public override string Name { get { return "工作状态报"; } }
+
+        public override int PType { get { return 0xe9; } }
+
+        public gw_stat_error error { get; set; }
+
+        public override int decode(byte[] data, int offset, out string msg)
+        {
+            int start = offset;
+            if (data.Length - offset < 5)
+                throw new Exception("数据缓冲区长度太小");
+            this.error = new gw_stat_error();
+            offset += gw_coding.GetTime(data,offset,out DateTime time);
+            error.Time = time;
+            offset += gw_coding.GetString(data, offset, data.Length - offset, out string str);
+            error.Desc = str;
+            msg = error.ToString();
+            this.Execute();
+            return offset - start;
+        }
+
+        public override byte[] encode(out string msg)
+        {
+            msg = string.Empty;
+            byte[] data = new byte[1];
+            int offset = 0;
+            data[offset++] = (byte)gw_ctrl.Status.Success;  //数据发送状态: 成功
+            return data;
+        }
+    }
+}
