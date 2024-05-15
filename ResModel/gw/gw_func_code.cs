4c2145c7d47e1afe758e3f7f6c2b7db0c7039660
rui he <herui13@126.com>
2024-05-15 22:01:55 +0800

    国网: 新增参数类型枚举(2015)

--- ResModel/gw/gw_func_code.cs
+++ ResModel/gw/gw_func_code.cs
@@ -13,4 +13,37 @@ namespace ResModel.gw
         [Description("覆冰数据")]
         ICE=0x22,
     }
+
+    /// <summary>
+    /// 国网参数类型枚举
+    /// </summary>
+    public enum gw_para_type
+    {
+        [Description("气象")]
+        Weather = 0xaf,
+
+        [Description("杆塔倾斜")]
+        Inclination = 0xb0,
+
+        [Description("导地线微风振动")]
+        Vibration = 0xb1,
+
+        [Description("导线弧垂")]
+        ConductorSag = 0xb2,
+
+        [Description("导线温度")]
+        LineTemperature = 0xb3,
+
+        [Description("覆冰")]
+        Ice = 0xb4,
+
+        [Description("导线风偏")]
+        WindageYaw = 0xb5,
+
+        [Description("导地线舞动")]
+        UGallop = 0xb6,
+
+        [Description("现场污秽度")]
+        Pollution = 0xb7,
+    }
 }
