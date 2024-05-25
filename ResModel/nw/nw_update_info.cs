feed68a605d4d94d95278e770d8e65f4d8675e91
rui he <herui13@126.com>
2024-05-25 15:25:53 +0800

    refactor nw_update_info to share code with gw

--- ResModel/nw/nw_update_info.cs
+++ ResModel/nw/nw_update_info.cs
@@ -1,7 +1,5 @@
-﻿using System.Text;
-using System.IO;
-using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using ResModel.gw_nw;
 
 namespace ResModel.nw
 {
@@ -25,47 +23,15 @@ namespace ResModel.nw
         UpdateFaile = 7,
     }
 
-    public class nw_update_info
+    public class nw_update_info : gn_update_info
     {
-        private string filename;
-
-        private string filepath;
-
         public nw_update_info() { 
-            this.MaxPacLength = 512;
             Password = "1234";
         }
 
         public string Password { get;set; }
 
         public int ChannelNO { get; set; }
-
-        public string FileName 
-        {
-            get { return this.filename; }
-            set { 
-                string name = value;
-                if (!FileNameCheck(name))
-                    throw new ArgumentException("文件名ASCII码可打印字符，不允许包含中文字符");
-                this.filename = name;
-            }
-        }
-
-        public string FilePath 
-        { 
-            get { return this.filepath; }
-            set { 
-                string path = value;
-                if(!File.Exists(path))
-                    throw new ArgumentException(string.Format("文件 \"{0}\" 不存在",path));
-                this.FileName = Path.GetFileName(path);
-                this.filepath = path;
-            } 
-        }
-
-        public int MaxPacLength { get;set; }
-
-        public FileStream stream { get; set; }
 
         public enum UpdateType
         {
@@ -81,75 +47,5 @@ namespace ResModel.nw
 
         public UpdateType Type { get; set; }
 
-        public int PacNum { get;set; }
-
-        public long FileLength { get; set; }
-
-        /// <summary>
-        /// 文件名检查
-        /// </summary>
-        /// <param name="filename"></param>
-        /// <returns></returns>
-        public bool FileNameCheck(string filename)
-        {
-            // ASCII码可打印字符，不允许包含中文字符
-            byte[] bytes = Encoding.Default.GetBytes(filename);
-            foreach(byte b in bytes)
-            {
-                // https://baike.baidu.com/item/isprint/6973194?fr=ge_ala
-                // 当c为可打印字符（0x20-0x7e）时，返回非零值，否则返回零。
-                if (b <0x20 || b >0x7f)
-                    return false;
-            }
-            return true;
-        }
-
-        public int GetPacNum()
-        {
-            if(this.FilePath == null)
-                return -1;
-            try
-            {
-                FileInfo fileInfo = new FileInfo(this.FilePath);
-                this.FileLength = fileInfo.Length;
-                int pnum = (int)(FileLength / this.MaxPacLength);
-                if (FileLength % this.MaxPacLength > 0)
-                    pnum++;
-                this.PacNum = pnum;
-                return pnum;
-
-            }
-            catch(Exception e)
-            {
-                return -1;
-            }
-        }
-
-        public byte[] GetPacData(int pno)
-        {
-            try
-            {
-                if (this.stream == null)
-                    this.stream = File.OpenRead(this.FilePath);
-
-                long offset = pno * this.MaxPacLength;
-                stream.Seek(offset, SeekOrigin.Begin);
-                byte[] buffer = new byte[this.MaxPacLength];
-                int length = stream.Read(buffer, 0, buffer.Length);
-                if(length < buffer.Length)
-                {
-                    byte[] temp = new byte[length];
-                    Buffer.BlockCopy(buffer,0,temp,0, length);
-                    return temp;
-                }
-                else
-                {
-                    return buffer;
-                }
-            }catch
-            {
-                return null;
-            }
-        }
     }
 }
