8fba6108cd653240b07d56b1f9d97ed5c4ddf879
rui he <herui13@126.com>
2024-06-12 22:32:58 +0800

    gw: remove unused class RealDataAyan.cs
    as weather data and ice date has mode to cms.service.gw_cmd

--- GridBackGround/PacketAnaLysis/PackDeal_Data.cs
+++ GridBackGround/PacketAnaLysis/PackDeal_Data.cs
@@ -26,57 +26,43 @@ namespace GridBackGround.PacketAnaLysis
             byte[] data,
             ref int errorCode)
         {
-            bool dealState = false;
-            if (packet_Type == (int)ICMP.Weather ||
-                packet_Type == (int)ICMP.Inclination ||
-                packet_Type == (int)ICMP.Ice)
+            switch (packet_Type)
             {
-                errorCode = 0;
-                try
-                {
-                    var ayan = new CommandDeal.Data.RealDataAyan(pole, packet_Type, data);
-                }
-                catch{}
+                //case PacketType_Monitoring.Weather:
+                //    CommandDeal.Data_Weather.Deal(pole, frame_No, data);
+                //    break;
+                case PacketType_Monitoring.Vibration_Character:
+                    CommandDeal.Data_ZD_Feature.Deal(pole, frame_No, data);
+                    break;
+                case PacketType_Monitoring.Vibration_Form:
+                    CommandDeal.Data_ZD_Form.Deal(pole, frame_No, data);
+                    break;
+                case PacketType_Monitoring.Conductor_Sag:
+                    CommandDeal.Data_Sag.Deal(pole, frame_No, data);
+                    break;
+                case PacketType_Monitoring.Conductor_Temperature:
+                    CommandDeal.Data_Line_Temperature.Deal(pole, frame_No, data);
+                    break;
+                case PacketType_Monitoring.Glaciation:
+                    CommandDeal.Data_Ice.Deal(pole, frame_No, data);
+                    break;
+                case PacketType_Monitoring.Conductor_Monsoon:
+                    CommandDeal.Data_FP.Deal(pole, frame_No, data);
+                    break;
+                case PacketType_Monitoring.Wave_Character:
+                    CommandDeal.Data_WD.Deal(pole, frame_No, data);
+                    break;
+                case PacketType_Monitoring.Wave_Trajectory:
+                    CommandDeal.Data_WD_Form.Deal(pole, frame_No, data);
+                    break;
+
+                default:
+                    errorCode = 0x05;
+                    break;
+
             }
-            else
-                switch (packet_Type)
-                {
-                    //case PacketType_Monitoring.Weather:
-                    //    CommandDeal.Data_Weather.Deal(pole, frame_No, data);
-                    //    break;
-                    case PacketType_Monitoring.Vibration_Character:
-                        CommandDeal.Data_ZD_Feature.Deal(pole, frame_No, data);
-                        break;
-                    case PacketType_Monitoring.Vibration_Form:
-                        CommandDeal.Data_ZD_Form.Deal(pole, frame_No, data);
-                        break;
-                    case PacketType_Monitoring.Conductor_Sag:
-                        CommandDeal.Data_Sag.Deal(pole, frame_No, data);
-                        break;
-                    case PacketType_Monitoring.Conductor_Temperature:
-                        CommandDeal.Data_Line_Temperature.Deal(pole, frame_No, data);
-                        break;
-                    case PacketType_Monitoring.Glaciation:
-                        CommandDeal.Data_Ice.Deal(pole, frame_No, data);
-                        break;
-                    case PacketType_Monitoring.Conductor_Monsoon:
-                        CommandDeal.Data_FP.Deal(pole, frame_No, data);
-                        break;
-                    case PacketType_Monitoring.Wave_Character:
-                        CommandDeal.Data_WD.Deal(pole, frame_No, data);
-                        break;
-                    case PacketType_Monitoring.Wave_Trajectory:
-                        CommandDeal.Data_WD_Form.Deal(pole, frame_No, data);
-                        break;
-
-                    default:
-                        errorCode = 0x05;
-                        break;
-
-                }
             //显示报文
-            dealState = OnDataPacketResPonse(pole.CMD_ID, packet_Type, frame_No);
-            return dealState;
+            return OnDataPacketResPonse(pole.CMD_ID, packet_Type, frame_No);
         }
 
         /// <summary>
