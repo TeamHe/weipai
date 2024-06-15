76dc52c4dace68ba9a57f9d645490843ed5b4433
rui he <herui13@126.com>
2024-06-15 17:28:57 +0800

    gw:  数据包解析调整为流式解析

--- FastSocket.Server/Command/CommandInfo_gw.cs
+++ FastSocket.Server/Command/CommandInfo_gw.cs
@@ -8,46 +8,6 @@ namespace Sodao.FastSocket.Server.Command
     /// </summary>
     public class CommandInfo_gw : ICommandInfo
     {
-        #region Constructors
-        /// <summary>
-        /// 报文内容
-        /// </summary>
-        /// <param name="CMD_ID">装置ID</param>
-        /// <param name="packet_Length">包长</param>
-        /// <param name="frame_Type">帧类型</param>
-        /// <param name="packet_Type">报文类型</param>
-        /// <param name="frame_No">帧序列号</param>
-        /// <param name="packet">通讯报文</param>
-        /// <param name="data">数据内容</param>
-        /// <param name="Crc">CRC校验码</param>
-        /// <param name="errorCode">错误代码</param>
-        public CommandInfo_gw(  string  CMD_ID,
-                                        int     packet_Length,
-                                        int     frame_Type,
-                                        int     packet_Type,
-                                        byte    frame_No,
-                                        byte[]  packet,                
-                                        byte[]  data,
-                                        byte[]  Crc,
-                                        int     errorCode)
-        {
-            this.CMD_ID = CMD_ID;
-            this.Packet_Lenth = packet_Length;
-            this.Frame_Type = frame_Type;
-            this.Packet_Type = packet_Type;
-            this.Frame_No = frame_No;
-            this.Packet = packet;
-            this.Data = data;
-            this.Crc = Crc;
-            this.ErrorCode = errorCode;
-        }
-
-        public CommandInfo_gw() 
-        {
-
-        }
-
-        #endregion
 
         #region Public Properties
         /// <summary>
@@ -116,125 +76,63 @@ namespace Sodao.FastSocket.Server.Command
         /// <returns></returns>
         public static CommandInfo_gw Find_commandinfo(ArraySegment<byte> buffer, out int readlength)
         {
-            int Packet_Lenth = 0;       //报文长度
-            string CMD_ID = "";         //状态监测装置ID
-            byte Frame_Type = 0;         //帧类型
-            byte Packet_Type = 0;       //报文类型
-            byte Frame_No = 0;
-            byte[] Pacekt = null;
-            byte[] data = null;                //报文内容
-            byte[] CRC = { 0x00 };         //CRC校验码
-            int erroCode = 0;
-
+            int startNo = -1;
             var payload = buffer.Array;
-            readlength = buffer.Count;
+            const int mlen = 27;      // 2 + 2 + 17 + 1 + 1 +1 + 2 + 1; //不包括报文内容
+            readlength = 0;
 
-            #region  数据长度校验
-            //[Sync][Packet_Lenth][CMD_ID][Frame_Type][Packet_Type][FrameNo][data][CRC16][End]
-            int MinLength = 27;      // 2 + 2 + 17 + 1 + 1 +1 + 2 + 1; //不包括报文内容
-            if (buffer.Count < MinLength)
-            {
-                readlength = 0;
+            if (buffer.Count - buffer.Offset < mlen)
                 return null;
-            }
-            #endregion
 
-            #region 寻找包头
-            //开始计数
-            int startNo = 0;
-            int i;
-            //找到包的起始位置
-            for (i = 0; i < buffer.Count - MinLength; i++)
+            for (int i = buffer.Offset; i < buffer.Count - mlen; i++)
             {
-                if ((payload[i + buffer.Offset] == 0xa5) && (payload[buffer.Offset + i + 1] == 0x5a))
+                if ((payload[i] == 0xa5) && (payload[i + 1] == 0x5a))
                 {
-                    startNo = i + buffer.Offset;
+                    startNo = i;
                     break;
                 }
+                readlength++;
             }
             //没找到包头
-            if (i == buffer.Count - MinLength)
-            {
-                erroCode = 0x02; //错误代码2
-                //数据长度小于最小包长，将数据提取并出来交给上层处理
-                Pacekt = new byte[buffer.Count - MinLength];
-                readlength = buffer.Count - MinLength;
+            if (startNo <0)
                 return null;
-
-            }
-            #endregion
-
-            #region 包长校验
 
             //计算包长
-            Packet_Lenth = (int)(payload[startNo + 2]) + (int)(payload[startNo + 3]) * 256;
-            if (Packet_Lenth > 2096)
+            int plen = (payload[startNo + 2]) | ((payload[startNo + 3]) << 8);
+            if (plen > 2096) //超长数据包检查
             {
-                //readlength = 4;
-                try
-                {
-                    //if(Pacekt == null)
-                    //    Pacekt = new byte[readlength];
-                    Buffer.BlockCopy(payload, startNo, Pacekt, 0, readlength);
-                    data = new byte[1];
-                    erroCode = 0x03; //错误代码8
-                    return new CommandInfo_gw(CMD_ID,
-                        Packet_Lenth,
-                        Frame_Type,
-                        Packet_Type,
-                        Frame_No,
-                        Pacekt,
-                        data,
-                        CRC,
-                        erroCode);
-                }
-                catch (Exception ex)
-                {
-                    string str = ex.Message;
-                }
-            }
-            Pacekt = new byte[Packet_Lenth + MinLength];
-            readlength = startNo - buffer.Offset + MinLength + Packet_Lenth;   //读数长度
-
-            if (buffer.Count < readlength)
-            {
-                readlength = 0;
+                readlength++;
                 return null;
             }
-            #endregion
+            //缓冲区长度不足，等待下次处理
+            if (plen +  mlen > buffer.Count - startNo)  
+                return null;
 
-            #region CRC校验
-            Buffer.BlockCopy(payload, startNo, Pacekt, 0, Packet_Lenth + MinLength);
-            CRC = CRC16.Crc(Pacekt, 2, Packet_Lenth + MinLength - 5);           //计算CRC16
-            int CRC_Position = Packet_Lenth + MinLength - 3;
-            if (!((CRC[0] == Pacekt[CRC_Position]) && CRC[1] == Pacekt[CRC_Position + 1]))        //CRC校验失败
+            int pcrc = plen + mlen - 3 + startNo;
+            byte[] CRC = CRC16.Crc(payload, 2 + startNo, plen + mlen - 5);           //计算CRC16
+            if (!((CRC[0] == payload[pcrc]) && CRC[1] == payload[pcrc + 1]))        //CRC校验失败
             {
-                erroCode = 0x04; //错误代码3
+                readlength++;
+                return null;
             }
-            #endregion
-            if (Pacekt[Packet_Lenth + MinLength - 1] != 0x96)
-            {
-                erroCode = 0x05;
-            }
-            #region 校验通过
+            readlength += plen + mlen;   //读数长度
 
-            CMD_ID = Encoding.UTF8.GetString(Pacekt, 4, 17);          //装置ID
-            Frame_Type = Pacekt[21];                                  //帧类型
-            Packet_Type = Pacekt[22];                                 //报文类型
-            Frame_No = Pacekt[23];                                    //帧序列号
-            data = new byte[Packet_Lenth];
-            int DataStart = 2 + 2 + 17 + 1 + 1 + 1;
-            Buffer.BlockCopy(Pacekt, DataStart, data, 0, Packet_Lenth);
-            return new CommandInfo_gw(CMD_ID,
-                    Packet_Lenth,
-                    Frame_Type,
-                    Packet_Type,
-                    Frame_No,
-                    Pacekt,
-                    data,
-                    CRC,
-                    erroCode);
-            #endregion
+            byte[] Pacekt = new byte[plen + mlen];
+            byte[] data = new byte[plen];
+            Buffer.BlockCopy(payload, startNo, Pacekt, 0, plen + mlen);
+            Buffer.BlockCopy(Pacekt, 24, data, 0, plen);
+
+            return new CommandInfo_gw()
+            {
+                CMD_ID = Encoding.UTF8.GetString(Pacekt, 4, 17),
+                Packet_Lenth = plen,
+                Frame_Type = Pacekt[21],
+                Packet_Type = Pacekt[22],
+                Frame_No = Pacekt[23],
+                Packet = Pacekt,
+                Data = data,
+                Crc = CRC,
+            };
         }
 
         /// <summary>
